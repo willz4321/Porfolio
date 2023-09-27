a3162ff8467db717e8c626a006e842ff06ac2a13
Facundo Suarez <willz4321@gmail.com>
2023-09-26 23:57:51 -0300

    subir journal

--- src/components/Proyect.jsx
+++ src/components/Proyect.jsx
@@ -7,16 +7,24 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 export const Proyect = () => {
   return (
     <>
-    
+   
        <Grid container 
             direction="column"
             justifyContent="center"
             alignItems="flex-start" 
             style={{gap: "30px", height: "100vh"}} id='portfolio'>
+
         <Typography variant='h2' style={{fontFamily: "initial" ,color: "#ffee58"}}>PORTFOLIO</Typography>
-        <Grid  container direction="row" justifyContent="flex-start" alignItems="center" >
+
+        <div style={{ overflowX: "auto", maxWidth: "75vw" }}>
+        <Grid  
+        container 
+        direction="row" 
+        justifyContent="flex-start" 
+        alignItems="center" 
+        style={{ width: "100vw", paddingBottom: '10px'}}>
         
-        <Card sx={{ maxWidth: 400, marginRight: '1rem', border: "solid white 1px" }} style={{height: "100%"}}>
+        <Card sx={{ maxWidth: 400, marginRight: '1rem', border: "solid white 1px" }} style={{height: "100%", }}>
             <CardMedia
             component="img"
             alt="pokedex"
@@ -106,8 +114,41 @@ export const Proyect = () => {
          
         </Card>
 
+        <Card sx={{ maxWidth: 400,marginRight: '1rem', border: "solid white 1px" }} style={{height: "100%" }}>
+            
+                <CardMedia
+                component="img"
+                alt="journal app"
+                height="150"
+                src="./assets/journal-app.png"
+                />
+                <CardContent style={{backgroundColor: "#481a75"}}>
+                <Typography gutterBottom variant="h5" component="div" style={{fontFamily: "Bebas Neue, cursive", color: "#ffee58"}}>
+                    Journal App
+                </Typography>
+                <Typography variant="body2" color="text.secondary" style={{fontFamily: "Bruno Ace", color: "#33c9dc", height: '140px'}}>
+                    Diario online desarrollada con react + spring con conexion a base de datos MongoDb, con login de usuario. Añade notas personales y sube imagenes a cloudinary.
+                </Typography>
+                </CardContent>
+                <CardActions style={{backgroundColor: "#481a75" ,justifyContent: "center"}}>
+                <FontAwesomeIcon icon={faHtml5} size="2xl" style={{color: "#f50a0a",}} />
+                <FontAwesomeIcon icon={faCss3Alt} size="2xl" style={{color: "#1160e8",}} />
+                <img src="./assets/logo-Material.png" alt="Material" style={{width: "40px", height: "40px"}} />
+                <FontAwesomeIcon icon={faReact} size="2xl" style={{color: "#0f3e8f",}} />
+                <img src="./assets/spring.png" alt="spring" style={{width: "60px", height: "40px"}} />
+                <img src="./assets/pngegg.png" alt="Mongo" style={{width: "40px", height: "40px"}} />
+                </CardActions>
+                <CardActions style={{backgroundColor: "#481a75" ,justifyContent: "center", paddingTop: "1px"}}>
+                <a underline="none" href="https://github.com/willz4321/journal-app-backend" target="_blank" rel="noopener"><GitHubIcon sx={{ color: "#ef6694" }} className='hover-icon'/> </a>
+                <a underline="none" href="https://github.com/willz4321/Tienda-web-backend" target="_blank" rel="noopener"><GitHubIcon sx={{ color: "black" }} className='hover-icon'/> </a>
+                </CardActions>
+         
+        </Card>
+
         </Grid>
+        </div>
     </Grid>
+   
 </>
   )
 }
